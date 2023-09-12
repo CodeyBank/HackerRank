00761f2de4ea10d1e4095ff8d928f09c551e9e11
Stanley Ezeh <ezeh.stanley.es@gmail.com>
2023-09-12 10:12:59 +0200

    Sets-STL

--- CPP/HR_BoundedValue/src/bounded.cpp
+++ CPP/HR_BoundedValue/src/bounded.cpp
@@ -58,13 +58,17 @@ int main()
     for (size_t i = 0; i < Q; i++)
     {
         int retval = GetIndexOfLowerBound(input, queries.at(i));
+        //auto retval = lower_bound(input.begin(), input.end(), queries.at(i));
         if (retval < 0)
+        //if (*retval != queries.at(i))
         {
-            cout << "No " << ((retval*-1) +1) << endl;
+            //cout << "No " << (retval-input.begin()) + 1 << endl;
+            cout << "No " << (retval * -1) + 1 << endl;
         }
         else
         {
-            cout << "Yes " << retval+1 << endl;
+            //cout << "Yes " << (retval - input.begin()) + 1 << endl;
+            cout << "No " << (retval) + 1 << endl;
         }
     }
     return 0;
@@ -73,58 +77,43 @@ int main()
 int GetIndexOfLowerBound(const vector<int> arr, int query)
 {
 
-    int indexOfLowerBound{0};
+    int indexOfLowerBound{0}, n=0;
     int leastGreaterNumber{100000000};
 
-    for (size_t i{0}; i < arr.size(); i++)
+    for each (const int var in arr)
     {
-        int tmp = arr.at(i);
-        // first check if the ith term is greater than the query
-        if (tmp > query)
+        n++;
+        if (var == query)
         {
-            if (tmp < leastGreaterNumber)
-            {
-                leastGreaterNumber = tmp;
-                indexOfLowerBound = i;
-            }
+            return n;
         }
-        // if query is found, return index of the query
-        else if (query == tmp)
+        if (var > query && var < leastGreaterNumber)
         {
-            return i;
+            leastGreaterNumber = var;
+            indexOfLowerBound = n;
         }
     }
+
+    //for (size_t i{0}; i < arr.size(); i++)
+    //{
+    //    int tmp = arr.at(i);
+    //    // first check if the ith term is greater than the query
+    //    if (tmp > query)
+    //    {
+    //        if (tmp < leastGreaterNumber)
+    //        {
+    //            leastGreaterNumber = tmp;
+    //            indexOfLowerBound = i;
+    //        }
+    //    }
+    //    // if query is found, return index of the query
+    //    else if (query == tmp)
+    //    {
+    //        return i;
+    //    }
+    //}
 
     // return a negative index of the least value greater than the query
     return (indexOfLowerBound) * -1;
 }
 
-int GetIndexOfLowerBound(const int arr[], int array_size, int query)
-{
-
-    int indexOfLowerBound{-100000}; // initialized index to impossible value
-    int leastGreaterNumber{100000}; // initialized to impossible value
-
-    for (size_t i{0}; i < array_size; i++)
-    {
-        int tmp = arr[i];
-        // first check if the ith term is greater than the query
-        if (tmp > query)
-        {
-            // update the leastGreaterNumber and corresponding index
-            if (tmp < leastGreaterNumber)
-            {
-                leastGreaterNumber = tmp;
-                indexOfLowerBound = i;
-            }
-        }
-        // if query is found, return index of the query
-        else if (query == tmp)
-        {
-            return i;
-        }
-    }
-
-    // check to se
-    return (indexOfLowerBound) * -1;
-}
---
+++ CPP/HR_Sets-STL/setstl.cc
@@ -0,0 +1,43 @@
+/**
+ * @file setstl.cc
+ * @author Stanley Ezeh (ezeh.stanley.es@gmail.com)
+ * @brief
+ * Each query is of one of the following three types:
+
+  : Add an element  to the set.
+  : Delete an element  from the set. (If the number  is not present in the set, then do nothing).
+  : If the number  is present in the set, then print "Yes"(without quotes) else print "No"(without quotes).
+
+Input Format
+
+The first line of the input contains  where  is the number of queries. The next  lines contain  query each.
+Each query consists of two integers  and  where  is the type of the query and  is an integer.
+
+Constraints
+
+Output Format
+
+For queries of type  print "Yes"(without quotes) if the number  is present in the set and if the number is not present,
+then print "No"(without quotes).
+Each query of type  should be printed in a new line.
+
+ * @version 0.1
+ * @date 2023-09-12
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
